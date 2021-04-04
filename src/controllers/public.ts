43de3b6a8008018a99d70fee052a0aa5ecd35935
backmeupplz <backmeupplz@gmail.com>
2021-04-04 08:01:09 -0700

    add user count route

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -2,11 +2,17 @@
 import { Context } from 'koa'
 import { Controller, Get } from 'koa-router-ts'
 import { stats } from '../helpers/stats'
+import { userCount } from '../helpers/userCount'
 
 @Controller('/')
 export default class {
   @Get('stats')
   stats(ctx: Context) {
     ctx.body = stats
+  }
+
+  @Get('count')
+  count(ctx: Context) {
+    ctx.body = userCount
   }
 }
